b08f73874c99919bc9b485ed196f02b2a75bf71e
Flor <florazul.fb@gmail.com>
2026-07-28 20:37:00 +0200

    feat: add clear cart action with useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useReducer } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
-import { useCart } from "./hooks/useCart";
+
 import { cartReducer, initialState } from "./reducers/cart-reducer";
 
 function App() {
-  const { clearCart } = useCart();
-
   const [state, dispatch] = useReducer(cartReducer, initialState);
   console.log(state);
 
   return (
     <>
-      <Header cart={state.cart} dispatch={dispatch} clearCart={clearCart} />
+      <Header cart={state.cart} dispatch={dispatch} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
